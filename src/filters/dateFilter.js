441b61237b7702850bcc57aa6cf61293d20ec94d
Daniil <lienglex@gmail.com>
2020-03-16 18:32:28 +0200

    added choice which type of options you need to

--- src/filters/dateFilter.js
+++ src/filters/dateFilter.js
@@ -1,9 +1,23 @@
-export default function dateFilter(value) {
-    const options = {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
+export default function dateFilter(value, format = 'date') {
+    let options = {};
+
+    if ( format.includes('time') ) {
+        options.hour =  'numeric'
+        options.minute =  'numeric'
+        options.second =  'numeric'
     }
+
+    if( format.includes('historyDate')) {
+        options.year =  'numeric'
+        options.month =  'numeric'
+        options.day = '2-digit'
+    }
+    
+    if ( format.includes('date') ) {
+        options.weekday =  'long'
+        options.year =  'numeric'
+        options.month =  'long'
+        options.day =  'numeric'
+    };
     return Intl.DateTimeFormat('ru-RU', options).format(value);
 }
